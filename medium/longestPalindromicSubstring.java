9588f383b26a4dbfa46ece8644fa65258ce0aefa
yashnil <yashnilmohanty@gmail.com>
2026-06-14 16:45:53 -0700

    up to date

---
+++ medium/longestPalindromicSubstring.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int start = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            
+            int len1 = expand(s, i, i); // odd length palindrome
+            int len2 = expand(s, i, i+1); // even length palindrome
+            
+            int temp = Math.max(len1, len2);
+            
+            if (temp > maxLength) {
+                maxLength = temp;
+                start = i - (temp-1)/2;
+            }
+        }
+
+        return s.substring(start, start + maxLength); 
+    }
+    private int expand(String s, int left, int right)
+    {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right))
+        {
+            left --;
+            right ++;
+        }
+
+        return right-left-1;
+    }
+}
---
+++ medium/stringToInteger.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int myAtoi(String s) {
+        int i = 0;
+
+        while (i < s.length() && s.charAt(i) == ' ') 
+        {
+            i++;
+        }
+
+        int sign = 1;
+
+        if (i < s.length() && s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        } 
+        else if (i < s.length() && s.charAt(i) == '+') {
+            i++;
+        }
+
+        int result = 0;
+
+        while (i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+            int digit = s.charAt(i) - '0';
+
+            if (result > Integer.MAX_VALUE / 10 ||
+                (result == Integer.MAX_VALUE / 10 && digit > 7)) 
+            {
+                if (sign == 1) 
+                {
+                    return Integer.MAX_VALUE;
+                } else 
+                {
+                    return Integer.MIN_VALUE;
+                }
+            }
+
+            result = result * 10 + digit;
+            i++;
+        }
+
+        return result * sign;
+    }
+}
